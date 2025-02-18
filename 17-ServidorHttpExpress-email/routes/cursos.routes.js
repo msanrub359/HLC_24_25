51db8ca30a1cf6f3f2e7cb8bcc0e7c9c019bb923
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2025-02-18 11:34:39 +0100

    Add files via upload

--- 17-ServidorHttpExpress-email/routes/cursos.routes.js
+++ 17-ServidorHttpExpress-email/routes/cursos.routes.js
@@ -3,17 +3,18 @@
 import { Router } from 'express';
 import { getCurso, getCursos,addCurso,updateCurso, updatePatchCurso, delCurso } from '../controllers/cursos.controllers.js';
 import { validar } from '../validators/cursos.validators.js';
-import { autenticarToken, autorizarRol } from '../controllers/auth.controller.js';
+import { autenticarToken, autorizarRol } from '../controllers/auth.controllers.js';
+
 
 const router = Router();
 
 
 router.get('/cursos', autenticarToken,  getCursos);
 //router.get('/cursos/:id', getAlumno);
-router.get('/cursos/:id', autenticarToken, getCurso);
+router.get('/cursos/:id',autenticarToken, getCurso);
 router.post('/cursos', autenticarToken, autorizarRol(['admin', 'user']),validar, addCurso);
 router.put('/cursos/:id', autenticarToken, autorizarRol(['admin', 'user']),validar, updateCurso);
 router.patch('/cursos/:id', autenticarToken, autorizarRol(['admin', 'user']),validar, updatePatchCurso);
 router.delete('/cursos/:id',  delCurso);
 
-export { router as routesCursos };
+export { router as routerCursos };
